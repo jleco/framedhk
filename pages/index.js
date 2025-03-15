70facefd302742d6fcd2f536e179efb3c2cf1a4d
jleco <jorge.m.lecona@gmail.com>
2025-03-15 10:18:57 +0800

    Initial commit - FramedHK setup

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      textAlign: "center",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1>Welcome to FramedHK</h1>
+      <p>This site is under construction.</p>
+    </div>
+  );
+}
